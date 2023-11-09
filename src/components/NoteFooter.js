dfef6ec21dcbb3285c3b05b3f369c15bbe17012f
aldiandarwin <aldiandarwinputra@gmail.com>
2023-11-09 20:57:09 +0700

    first commit

---
+++ src/components/NoteFooter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NoteFooter() {
+    return (
+        <div className='note-app__footer'>
+            <p>Copyright &#169; 2023, aldian darwin </p>
+        </div>
+    );
+}
+
+export default NoteFooter;
